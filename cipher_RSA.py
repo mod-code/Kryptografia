b2b3e06ce6869d2d0e10ed20b3a133342e9ee9ca
Wojciech S <strzebonsky@gmail.com>
2020-04-27 14:35:32 +0200

    Pierwsza wersja ustalania pary kluczy ale chyba nie do konca poprawna

--- cipher_RSA.py
+++ cipher_RSA.py
@@ -13,6 +13,10 @@ class GcdChecker:
             self.firstNumber, self.secondNumber = self.secondNumber, self.firstNumber % self.secondNumber
         return self.firstNumber
 
+    def set_numbers(self, firstNumber, secondNumber):
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+
 
 class PrimeChecker:
     def __init__(self, number):
@@ -35,6 +39,38 @@ class PrimeChecker:
 
 
 class RSA:
-    def __init__(self):
-        self.p = None
-        self.q = None
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+
+    def set_p_q(self, p, q):
+        self.p = p
+        self.q = q
+
+    def generate_keypair(self):
+        first_number_checker = PrimeChecker(self.p)
+        second_number_checker = PrimeChecker(self.q)
+
+        if not (first_number_checker.check_number() and second_number_checker.check_number()):
+            raise ValueError('ERROR: Both numbers must be prime!')
+        elif self.p == self.q:
+            raise ValueError('ERROR: p and q cannot be equal!')
+
+        n = self.p * self.q
+        phi_n = (self.p - 1) * (self.q - 1)
+
+        e = random.randrange(1, phi_n)
+
+        gcd_checker = GcdChecker(e, phi_n)
+        coprime = gcd_checker.check_numbers()
+        while coprime != 1:
+            e = random.randrange(1, phi_n)
+            gcd_checker.set_numbers(e, phi_n)
+            coprime = gcd_checker.check_numbers()
+
+        k = 2
+
+        d = (1 + k * phi_n) / e
+
+        return (e, n), (d, n)
+
