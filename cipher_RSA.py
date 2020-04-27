d5206c497b3ee3682be27c0f0bac6393569d5cd7
Wojciech S <strzebonsky@gmail.com>
2020-04-27 15:12:32 +0200

    Wprowadzono poprawne generowanie pary kluczy

--- cipher_RSA.py
+++ cipher_RSA.py
@@ -37,6 +37,24 @@ class PrimeChecker:
         else:
             return False
 
+class EEA:
+    def __init__(self, first_number, second_number):
+        self.first_number = first_number
+        self.second_number = second_number
+
+    def calculate(self):
+        p0, p1, a0, n0 = 0, 1, self.first_number, self.second_number
+        q, r = n0 // a0, n0 % a0
+        while r:
+            t = p0 - q * p1
+            if t >= 0:
+                t = t % self.second_number
+            else:
+                t = self.second_number - ((-t) % self.second_number)
+            p0, p1, n0, a0 = p1, t, a0, r
+            q, r = n0 // a0, n0 % a0
+        return p1
+
 
 class RSA:
     def __init__(self, p, q):
@@ -46,6 +64,7 @@ class RSA:
     def set_p_q(self, p, q):
         self.p = p
         self.q = q
+
 
     def generate_keypair(self):
         first_number_checker = PrimeChecker(self.p)
@@ -68,9 +87,9 @@ class RSA:
             gcd_checker.set_numbers(e, phi_n)
             coprime = gcd_checker.check_numbers()
 
-        k = 2
-
-        d = (1 + k * phi_n) / e
+        EEAchecker = EEA(e, phi_n)
+        d = EEAchecker.odwr_mod()
 
         return (e, n), (d, n)
+
 
