6f632f48751d5647d7260d3120e31798bc4c02af
Wojciech S <strzebonsky@gmail.com>
2020-04-27 13:53:31 +0200

    Utworzono klasy do sprawdzania NWD i liczb pierwszych. Zmodyfikowano algorytm sprawdzania czy liczba jest liczbą pierwszą

--- cipher_RSA.py
+++ cipher_RSA.py
@@ -1,21 +1,40 @@
+import math
 import random
 
 
-# Algorytm Euklidesa - odszukanie największego wspólnego dzielnika
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+class GcdChecker:
+    def __init__(self, firstNumber, secondNumber):
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+
+    # Sprawdzenie największego wspólnego dzielnika dwóch liczb - Algorytm Euklidesa
+    def check_numbers(self):
+        while self.secondNumber != 0:
+            self.firstNumber, self.secondNumber = self.secondNumber, self.firstNumber % self.secondNumber
+        return self.firstNumber
 
 
-# Algorytm sprawdzający czy liczba jest liczbą pierwszą
-def isPrime(number):
-    if number > 1:
-        for i in range(2, number//2):
-            if number % i == 0:
+class PrimeChecker:
+    def __init__(self, number):
+        self.number = number
+
+    # Sprawdzenie czy podana liczba jest pierwsza
+    def check_number(self):
+        # Czy liczba jest dodatnia?
+        if self.number > 1:
+            # czy liczba jest parzysta i większa od 2?
+            if self.number % 2 == 0 and self.number > 2:
                 return False
-        else:
+            # sprawdzaj dzielnik do pierwiastka z liczb i co drugi krok (nie trzeba już parzystych sprawdzać)
+            for i in range(3, int(math.sqrt(self.number)) + 1, 2):
+                if self.number % i == 0:
+                    return False
             return True
-    else:
-        return False
+        else:
+            return False
 
+
+class RSA:
+    def __init__(self):
+        self.p = None
+        self.q = None
